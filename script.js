9c795c2b20d5cb455c217d5179ce5b6f572050db
RafaelMiranda21 <38355955+RafaelMiranda21@users.noreply.github.com>
2020-09-03 19:22:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+
+let logoRobofoguete = document.getElementById("Logo-robo-foguete");
+let chatRobo = document.getElementById("chat-robo");
+
+
+// window.addEventListener("scroll" , function() {
+//     let value = window.scrollY;
+//     let opacity;
+//      logoRobofoguete.style.left = value * .5 + "px";
+
+// });
+
+
+let Hamburguer = document.querySelector(".hambuguer");
+let Sidebar = document.querySelector(".container");
+
+Hamburguer.addEventListener("click" , () =>{
+    Sidebar.classList.toggle("change");
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(document).ready(function(){
+//     $("a").on('click', function(event) {
+//       if (this.hash !== "") {
+//         event.preventDefault();
+//         var hash = this.hash;
+//         $('html, body').animate({
+//           scrollTop: $(hash).offset().top
+//         }, 2000, function(){
+//           window.location.hash = hash;
+//         });
+//       } 
+//     });
+//   });
+
+  
+
